8858525d97b74369ba290dcfa837bda34d2d2057
Ds8880 <dhruvshingala8880@gmail.com>
2025-03-07 17:05:26 +0530

    first commit

---
+++ routes/route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/controller');
+
+const { authentication, authorize } = require('../middleware/auth');
+
+router.post('/register', controller.register);
+router.post('/login', controller.login);
+router.get('/logout', controller.logout);
+router.get('/', authentication, authorize, controller.getAllCustomersData);
+router.get('/cus', authentication, controller.getCustomerData);
+
+module.exports = router; 
