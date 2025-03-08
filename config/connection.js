7cd549f72dba99ae65984ce3c85426e8f4911611
Ds8880 <dhruvshingala8880@gmail.com>
2025-03-08 16:54:12 +0530

    small changes

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = () => {
     mongoose.connect(process.env.MONGO_URI)
-        .then(() => console.log('Connected successfully'))
+        .then((data) => console.log('Connected successfully'))
         .catch(err => console.error('No Connected!', err));
 }
 
